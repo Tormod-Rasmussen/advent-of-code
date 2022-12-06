63f240db5a60995c1b8d4418fbd4402db0acdd08
Tormod-Rasmussen <tormodrasmussen+github.com@gmail.com>
2022-12-06 09:41:45 +0100

    Update day2.py

--- 2/day2.py
+++ 2/day2.py
@@ -1,3 +1,4 @@
+input = open("2/input.txt").readlines()
 outcomes = {
     "A X":4,
     "A Y":8,
@@ -9,9 +10,7 @@ outcomes = {
     "C Y":2,
     "C Z":6
 }
-score = 0
-with open("2/input.txt") as f:
-    score = sum(outcomes[line[:3]] for line in f)
+score = sum(outcomes[line[:3]] for line in input)
 print("part 1:",score)
 outcomes = {
     "A X":3,
@@ -24,6 +23,5 @@ outcomes = {
     "C Y":6,
     "C Z":7
 }
-with open("2/input.txt") as f:
-    score = sum(outcomes[line[:3]] for line in f)
+score = sum(outcomes[line[:3]] for line in input)
 print("part 2:",score)
