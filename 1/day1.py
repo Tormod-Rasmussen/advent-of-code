86e7ebf3146dc931e20ce9a86d8a4ea3e81d14ed
Tormod-Rasmussen <tormodrasmussen+github.com@gmail.com>
2022-12-06 09:42:07 +0100

    Update day1.py

--- 1/day1.py
+++ 1/day1.py
@@ -1,14 +1,13 @@
 bag = []
 temp = 0
-with open("1/input.txt") as f:
-    data = f.read().splitlines()
-    data.append("")
-    for line in data:
-        if line != "":
-            temp += int(line)
-        else:
-            bag.append(temp)
-            temp = 0
+lines = open("1/input.txt").read().splitlines()
+lines.append("")
+for line in lines:
+    if line != "":
+        temp += int(line)
+    else:
+        bag.append(temp)
+        temp = 0
 bag.sort()
 print("part 1:",bag[-1])
 print("part 2:",sum(bag[-3:]))
