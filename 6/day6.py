ee7901928484ea5a2a075aba24246a8e1c585f2c
Tormod-Rasmussen <tormodrasmussen+github.com@gmail.com>
2022-12-06 09:39:48 +0100

    Update day6.py

--- 6/day6.py
+++ 6/day6.py
@@ -1,9 +1,9 @@
-input = open("6/input.txt").read()
-for i in range(len(input)):
-    if len(set(input[i:i+4])) == 4:
+string = open("6/input.txt").read()
+for i in range(len(string)):
+    if len(set(string[i:i+4])) == 4:
         print("part 1:", i+4)
         break
-for i in range(len(input)):
-    if len(set(input[i:i+14])) == 14:
+for i in range(len(string)):
+    if len(set(string[i:i+14])) == 14:
         print("part 2:", i+14)
         break
