da370f5221c46f585b087ec411ed5ebcb6fcda79
Tormod-Rasmussen <tormodrasmussen+github.com@gmail.com>
2022-12-16 09:30:06 +0100

    day 13 part 2

--- 13/day13.py
+++ 13/day13.py
@@ -1,6 +1,6 @@
 signals = open("13/input.txt").read().split("\n\n")
 
-def compare(left,right,top): # returns True if right is bigger than left
+def compare(left,right,top): # top is False if comparing from within the function
     for i in range(max(len(right), len(left))):
         try:
             if type(left[i]) == int and type(right[i]) == int:
@@ -32,7 +32,7 @@ def compare(left,right,top): # returns True if right is bigger than left
             else:
                 # print("left side ran out of items when comparing:", left," and ", right)
                 return True
-    return True if top else "cont" # if comparing from main return True, else continue comparing
+    return True if top else "" # if comparing from outside function return True, else continue comparing
 
 rightOrder = 0
 for pair in range(len(signals)):
@@ -40,3 +40,15 @@ for pair in range(len(signals)):
     left, right = eval(left), eval(right)
     rightOrder += pair+1 if compare(left, right,1) else 0
 print("part 1:",rightOrder)
+
+ordered = [[[2]],[[6]]]
+for line in open("13/input.txt").read().split("\n"):
+    if line != "": # add all packets to list
+        ordered += [eval(line)]
+for i in range(len(ordered)): # compare all packets to each other
+    for j in range(len(ordered)):
+        if compare(ordered[i],ordered[j],0) == True:        # if right is bigger than left
+            ordered[i], ordered[j] = ordered[j], ordered[i] # swap
+dividerPackets = (ordered.index([[2]])+1) * (ordered.index([[6]])+1)
+
+print("part 2:",dividerPackets)
