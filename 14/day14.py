35eafab7816fcf9f7ef6533dd046a9d101958c21
Tormod-Rasmussen <tormodrasmussen+github.com@gmail.com>
2022-12-16 09:27:32 +0100

    Add files via upload

--- 14/day14.py
+++ 14/day14.py
@@ -2,7 +2,7 @@ lines = open("14/input.txt").read().splitlines()
 def makeGrid():
     grid = {}
     for y in range(170):            # 170 tall
-        for x in range(0, 1000):   # width between 0 and 1000
+        for x in range(250,750):   # width between 250 and 750
             grid[(x, y)] = "."
     for line in lines:
         line = line.split(" -> ")
@@ -22,21 +22,16 @@ def makeGrid():
                 while x1 <= x2:
                     grid[(x1, y1)] = "#"
                     x1 += 1
-        lowestRock = 0 # lowest rock formation (highest y value)
-        for y in range(170):
-            for x in range(0, 1000):
-                if grid[(x, y)] == "#":
-                    lowestRock = max(lowestRock, y)
-    return grid, lowestRock
+    return grid
 
 def sandFalling():
     sandAmount = 0 # amount of sand
-    sandPos = (500, 0)
+    sandPos = (500, 0) # spawn sand at 500,0
     while sandPos[1] < 169: # while sand is in grid
-        if grid[sandPos[0],sandPos[1]+1] != ".": # if something is under, check diagonal left
-            if grid[sandPos[0]-1,sandPos[1]+1] != ".": # if something is diagonal left, check diagonal right
-                if grid[sandPos[0]+1,sandPos[1]+1] != ".": # if something is diagonal right, place sand
-                    if sandPos == (500,0): #sand entrence blocked
+        if grid[sandPos[0],sandPos[1]+1] != ".": # if it can't go down, check diagonals
+            if grid[sandPos[0]-1,sandPos[1]+1] != ".": # if it can't go diagonal left
+                if grid[sandPos[0]+1,sandPos[1]+1] != ".": # if it can't go diagonal right
+                    if sandPos == (500,0): # if the sand entrence is blocked
                         return sandAmount+1 # including the one at 500,0
                     grid[sandPos] = "o" # place sand on grid
                     sandAmount += 1 # increment sand amount
@@ -44,12 +39,21 @@ def sandFalling():
                 else: sandPos = (sandPos[0]+1, sandPos[1]+1) # nothing diagonal right, move sand diagonal right
             else: sandPos = (sandPos[0]-1, sandPos[1]+1) # nothing diagonal left, move sand diagonal left
         else: sandPos = (sandPos[0], sandPos[1]+1) # nothing under, move sand down
-    return sandAmount # return amount of sand that fell
+    return sandAmount # return amount of sand
 
-grid,lowestRock = makeGrid()
+grid = makeGrid()
 print("part 1:",sandFalling())
 
-grid = makeGrid()[0] # remove sand from part 1
-for x in range(0, 1000): # add floor to grid
-    grid[(x, lowestRock+2)] = "#"
+grid = makeGrid() # remake grid to remove sand
+lowestRock = 0 # lowest rock formation (highest y value)
+for y in range(169,0,-1):
+    for x in range(250,750):
+        if grid[(x, y)] == "#":
+            lowestRock = y
+            break
+    if lowestRock != 0:
+        break
+
+for x in range(250,750):
+    grid[(x, lowestRock+2)] = "#"# add floor to grid
 print("part 2:",sandFalling())
