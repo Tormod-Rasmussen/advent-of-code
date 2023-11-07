74911c24723965f405a50b2a04c48b88beaa5bea
Tormod-Rasmussen <tormodrasmussen@gmail.com>
2023-11-07 13:48:59 +0100

    2017 #8 I Heard You Like Registers

---
+++ 2017/08/day8.py
@@ -0,0 +1,20 @@
+#--- Day 8: I Heard You Like Registers ---
+
+with open('2017/08/input.txt') as f:
+    variables = {name:0 for name in [line.strip().split()[0] for line in f]}
+    
+with open('2017/08/input.txt') as f:
+    lines = f.read().splitlines()
+
+highest_value = 0
+for line in lines:
+    line = line.split()
+    if eval(f'{variables[line[4]]} {line[5]} {line[6]}'):
+        if line[1] == 'inc':
+            variables[line[0]] += int(line[2])
+        elif line[1] == 'dec':
+            variables[line[0]] -= int(line[2])
+        highest_value = max(highest_value,variables[line[0]])
+
+print('part 1:',max(variables.values()))
+print('part 2:',highest_value)
