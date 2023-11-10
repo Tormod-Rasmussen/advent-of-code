971afc1e72de06b79f6511ed7483f4f7f321c18e
Tormod-Rasmussen <tormodrasmussen@gmail.com>
2023-11-10 13:21:11 +0100

    Update day17.py

--- 2015/17/day17.py
+++ 2015/17/day17.py
@@ -6,17 +6,17 @@ with open('2015/17/input.txt') as f:
 lines = [int(i) for i in lines]
 litres = 150
 total = 0
-smallest_num_containers = len(lines)
 for i in range(len(lines)):
     for combo in combinations(lines,i):
         if sum(combo) == litres:
-            smallest_num_containers = min(smallest_num_containers,len(combo))
             total += 1
 print('part 1:',total)
 
 total = 0
 for i in range(len(lines)):
     for combo in combinations(lines,i):
-        if sum(combo) == litres and len(combo) == smallest_num_containers:
+        if sum(combo) == litres:
             total += 1
+    if total != 0:
+        break
 print('part 2:',total)
